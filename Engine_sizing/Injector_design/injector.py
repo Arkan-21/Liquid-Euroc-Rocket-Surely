b8b805359ad5dbe4270c4c2d7482fd5ff3d76442
Nita <arkansow@gmail.com>
2026-04-17 16:50:26 +0400

    config

--- Engine_sizing/Injector_design/injector.py
+++ Engine_sizing/Injector_design/injector.py
@@ -1,2 +1,14 @@
-import os
-os.system("gmsh -version")
+import numpy as np
+import cea
+
+class Injector_Sizing:
+    def __init__(self, fuel, ox, Pc, MR, expansion_ratio, injection_velocity, injector_diameter, m_dot, m_dot_film):
+        self.fuel = fuel
+        self.ox = ox
+        self.Pc = Pc
+        self.MR = MR
+        self.expansion_ratio = expansion_ratio
+        self.injection_velocity = injection_velocity
+        self.injector_diameter = injector_diameter
+        self.m_dot = m_dot
+        self.m_dot_film = m_dot_film
